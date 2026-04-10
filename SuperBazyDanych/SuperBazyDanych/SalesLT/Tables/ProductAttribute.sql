6f0837a045d466c2a294d2d606601a140913f413
Emilia Delimata <delimataemilia@gmail.com>
2026-04-10 16:19:35 +0200

    zadanie 6 i poczatek 7

---
+++ SuperBazyDanych/SuperBazyDanych/SalesLT/Tables/ProductAttribute.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [SalesLT].[ProductAttribute] (
+    ProductID   int PRIMARY KEY,
+    Info        xml(ProductSchema) NULL
+
+    constraint fkpap foreign key (ProductID)
+    references [SalesLT].[Product] (ProductID)
+);
---
+++ SuperBazyDanych/SuperBazyDanych/dbo/XMLSchemaCollections/ProductSchema.sql
@@ -0,0 +1,16 @@
+﻿-- =============================================
+-- Zadanie 6
+-- =============================================
+CREATE XML SCHEMA COLLECTION ProductSchema AS N'
+<xs:schema xmlns:xs="http://www.w3.org/2001/XMLSchema">
+  <xs:element name="Product" type="ProductType"/>
+  <xs:complexType name="ProductType">
+    <xs:sequence>
+      <xs:element name="Name" type="xs:string"/>
+      <xs:element name="Price" type="xs:decimal"/>
+      <xs:element name="Weight" type="xs:decimal"/>
+      <xs:element name="NumberOfElements" type="xs:decimal"/>
+      <xs:element name="MainMaterial" type="xs:string"/>
+    </xs:sequence>
+  </xs:complexType>
+</xs:schema>';
