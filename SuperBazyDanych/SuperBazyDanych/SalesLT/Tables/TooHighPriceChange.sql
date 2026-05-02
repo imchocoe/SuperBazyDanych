b088eb321b0b02167d0684c46d9d6fa4d5c9664c
Emilia <delimataemilia@gmail.com>
2026-05-02 19:34:20 +0200

    lab-8 zad 4

---
+++ SuperBazyDanych/SuperBazyDanych/SalesLT/Tables/TooHighPriceChange.sql
@@ -0,0 +1,11 @@
+﻿-- =============================================
+-- Zadanie 4
+-- =============================================
+CREATE TABLE SalesLT.TooHighPriceChange (
+    ChangeID         INT IDENTITY PRIMARY KEY,
+    ProductID       INT,
+    OldPrice        money,
+    NewPrice        money,
+    ChangedBy       SYSNAME      DEFAULT SYSTEM_USER,
+    ChangedAt       DATETIME2    DEFAULT SYSDATETIME()
+);
