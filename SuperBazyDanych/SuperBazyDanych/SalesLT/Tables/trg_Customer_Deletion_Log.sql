58ea49834f82bf54796ed62cc0352d5f3eb35d20
Emilia <delimataemilia@gmail.com>
2026-05-02 15:42:26 +0200

    lab-8 zad 2

---
+++ SuperBazyDanych/SuperBazyDanych/SalesLT/Tables/trg_Customer_Deletion_Log.sql
@@ -0,0 +1,18 @@
+﻿CREATE TRIGGER trg_Customer_Deletion_Log
+ON SalesLT.Customer
+instead of delete
+AS
+BEGIN
+    INSERT INTO SalesLT.DeletedCustomersLog (CustomerID,FirstName,LastName,DateOfDeletion)
+    SELECT CustomerID,FirstName,LastName,getdate()
+    FROM deleted d
+    where exists (select 1 from SalesLT.SalesOrderHeader soh
+    where soh.CustomerID=d.CustomerID)
+
+    delete c
+    from SalesLT.Customer c
+    inner join deleted d on c.CustomerID = d.CustomerID
+    where not exists (select 1 from SalesLT.SalesOrderHeader soh
+    where soh.CustomerID=d.CustomerID)
+
+END;
