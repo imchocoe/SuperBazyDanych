1e64e982803ff938ef39c8cf2f8c24fc52543c60
Emilia <delimataemilia@gmail.com>
2026-05-25 17:47:33 +0200

    lab-11 male poprawki

--- SuperBazyDanych/SuperBazyDanych/dbo/Tables/BazaSalonuSamochodowego.sql
+++ SuperBazyDanych/SuperBazyDanych/dbo/Tables/BazaSalonuSamochodowego.sql
@@ -9,6 +9,22 @@
 -- =============================================
 -- Zadanie 4
 -- =============================================
+-- =============================================
+-- Zadanie 2
+-- =============================================
+
+
+-- =============================================
+-- Zadanie 3
+-- =============================================
+
+-- =============================================
+-- Zadanie 4
+-- =============================================
+--Właściciel małego salonu samochodowego chce na bieżąco monitorować swój stan magazynowy. Potrzebuje raportu, który w jednym widoku pokaże mu trzy rzeczy: 
+--dokładną liczbe sztuk konkretnego modelu, podsumowanie częściowe dla danej marki oraz całkowita liczbę samochodów.
+
+
 create table dbo.BazaSalonuSamochodowego(
     IDModelu int identity primary key,
     Marka nvarchar(50),
